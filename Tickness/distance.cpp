6fa295e5c4ba734f947b1743a6cfb5698e14b905
Gustavo Canavaci Barizon <gb@fo5.com>
2017-04-25 23:52:09 -0300

    implementação e ajustes na extração de espessura miocárdica (8 direções)

--- Tickness/distance.cpp
+++ Tickness/distance.cpp
@@ -101,22 +101,26 @@ void Distance::GetTickness(int first, int last){
             distance[1] = 0.0;
             distance[2] = 0.0;
             distance[3] = 0.0;
+            distance[4] = 0.0;
+            distance[5] = 0.0;
+            distance[6] = 0.0;
+            distance[7] = 0.0;
             distance[0] = calcDistance(val, x, y, 0);
             distance[1] = calcDistance(val, x, y, 1);
             distance[2] = calcDistance(val, x, y, 2);
             distance[3] = calcDistance(val, x, y, 3);
-            //            distance[4] = calcDistance(val, first, last, 4);
-            //            distance[5] = calcDistance(val, first, last, 5);
-            //            distance[6] = calcDistance(val, first, last, 6);
-            //            distance[7] = calcDistance(val, first, last, 7);
+            distance[4] = calcDistance(val, x, y, 4);
+            distance[5] = calcDistance(val, x, y, 5);
+            distance[6] = calcDistance(val, x, y, 6);
+            distance[7] = calcDistance(val, x, y, 7);
             extractValues<<"Pos 0 - Tickness"<<i+1<<": "<<distance[0]<<endl;
             extractValues<<"Pos 1 - Tickness"<<i+1<<": "<<distance[1]<<endl;
             extractValues<<"Pos 2 - Tickness"<<i+1<<": "<<distance[2]<<endl;
             extractValues<<"Pos 3 - Tickness"<<i+1<<": "<<distance[3]<<endl;
-            //            extractValues<<"Pos 4 - Tickness"<<i+1<<": "<<distance[4]<<endl;
-            //            extractValues<<"Pos 5 - Tickness"<<i+1<<": "<<distance[5]<<endl;
-            //            extractValues<<"Pos 6 - Tickness"<<i+1<<": "<<distance[6]<<endl;
-            //            extractValues<<"Pos 7 - Tickness"<<i+1<<": "<<distance[7]<<endl;
+            extractValues<<"Pos 4 - Tickness"<<i+1<<": "<<distance[4]<<endl;
+            extractValues<<"Pos 5 - Tickness"<<i+1<<": "<<distance[5]<<endl;
+            extractValues<<"Pos 6 - Tickness"<<i+1<<": "<<distance[6]<<endl;
+            extractValues<<"Pos 7 - Tickness"<<i+1<<": "<<distance[7]<<endl;
             extractValues<<"--------------------------------"<<endl;
         }
     }
@@ -144,7 +148,7 @@ double Distance::calcDistance(ImageType2D::Pointer image, int x, int y, int pos)
     itk::Point<int,2> p0;
     itk::Point<int,2> p1;
     if(pos == 0){
-        for(int b= y; b<region[1]; b = b - 1){
+        for(int b= y; b > 0; b = b - 1){
             const ImageType::IndexType index = {{x,b}};
             if(((x) >= 0) && ((x) <= region[0]) && ((b) >= 0) && ((b) <= region[1])){
                 if(image->GetPixel(index) > 0 && !isElement){
@@ -178,7 +182,7 @@ double Distance::calcDistance(ImageType2D::Pointer image, int x, int y, int pos)
     }
 
     if(pos == 2){
-        for(int b= x; b<region[0]; b = b - 1){
+        for(int b= x; b > 0; b = b - 1){
             const ImageType::IndexType index = {{b,y}};
             if(((y) >= 0) && ((y) <= region[1]) && ((b) >= 0) && ((b) <= region[0])){
                 if(image->GetPixel(index) > 0 && !isElement){
@@ -211,8 +215,97 @@ double Distance::calcDistance(ImageType2D::Pointer image, int x, int y, int pos)
         }
     }
 
-    cout<<"Value = "<<p0.EuclideanDistanceTo(p1)<<endl;
-    return p0.EuclideanDistanceTo(p1);
+    if(pos == 4){
+        int b = y;
+        for(int a= x; a<region[0]; a = a + 1){
+            if(b+1 < region[1]){
+                const ImageType::IndexType index = {{a+1,b+1}};
+                if(((a+1) >= 0) && ((a+1) <= region[0]) && ((b+1) >= 0) && ((b+1) <= region[1])){
+                    if(image->GetPixel(index) > 0 && !isElement){
+                        p0[0] = a;
+                        p0[1] = b;
+                        isElement = true;
+                    }
+                    if(isElement && image->GetPixel(index) > 0){
+                        p1[0] = a;
+                        p1[1] = b;
+                    }
+                }
+            }
+            b++;
+        }
+    }
+
+    if(pos == 5){
+        int b = y;
+        for(int a= x; a>0; a = a - 1){
+            if(b+1 < region[1]){
+                const ImageType::IndexType index = {{a-1,b+1}};
+                if(((a-1) >= 0) && ((a-1) <= region[0]) && ((b+1) >= 0) && ((b+1) <= region[1])){
+                    if(image->GetPixel(index) > 0 && !isElement){
+                        p0[0] = a;
+                        p0[1] = b;
+                        isElement = true;
+                    }
+                    if(isElement && image->GetPixel(index) > 0){
+                        p1[0] = a;
+                        p1[1] = b;
+                    }
+                }
+            }
+            b++;
+        }
+    }
+
+    if(pos == 6){
+        int a = x;
+        for(int b=y; b>0; b = b - 1){
+            if(a-1 > 0){
+                const ImageType::IndexType index = {{a-1,b-1}};
+                if(((a-1) >= 0) && ((a-1) <= region[0]) && ((b-1) >= 0) && ((b-1) <= region[1])){
+                    if(image->GetPixel(index) > 0 && !isElement){
+                        p0[0] = a;
+                        p0[1] = b;
+                        isElement = true;
+                    }
+                    if(isElement && image->GetPixel(index) > 0){
+                        p1[0] = a;
+                        p1[1] = b;
+                    }
+                }
+            }
+            a--;
+        }
+    }
+
+    if(pos == 7){
+        int a = x;
+        for(int b=y; b>0; b = b - 1){
+            if(a+1 <region[0]){
+                const ImageType::IndexType index = {{a+1,b-1}};
+                if(((a+1) >= 0) && ((a+1) <= region[0]) && ((b-1) >= 0) && ((b-1) <= region[1])){
+                    if(image->GetPixel(index) > 0 && !isElement){
+                        p0[0] = a;
+                        p0[1] = b;
+                        isElement = true;
+                    }
+                    if(isElement && image->GetPixel(index) > 0){
+                        p1[0] = a;
+                        p1[1] = b;
+                    }
+                }
+            }
+            a++;
+        }
+    }
+
+    if(p0.EuclideanDistanceTo(p1) > 99999){
+        return 0.0;
+    }
+    else{
+        return p0.EuclideanDistanceTo(p1);
+    }
+
 }
 
 
